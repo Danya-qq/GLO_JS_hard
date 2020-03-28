be892bbb21af8925e6c796e499feb035792407a0
Danya-qq <qallandbb@gmail.com>
2020-03-28 17:04:13 +0300

    lesson2_hard is done

---
+++ script.js
@@ -0,0 +1,19 @@
+let num = 266219;
+
+let result = 1;
+num = String(num);  // преобразование числа в строку
+   for (let elem of num) {
+    result *= lem;
+};
+
+console.log(result);
+
+result **= 3;
+console.log(result);
+
+let str = String(result);
+str = str.slice(0,2);
+
+console.log(str);
+
+
