9f516e529637dff35a83f72cd52fcf4efca31fde
Danya-qq <qallandbb@gmail.com>
2020-03-29 14:31:08 +0300

    modified JS code

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 let num = 266219;
 
-let result = 1;
-num = String(num);  // преобразование числа в строку
-   for (let elem of num) {
-    result *= lem;
-};
-
-console.log(result);
-
-result **= 3;
-console.log(result);
-
-let str = String(result);
-str = str.slice(0,2);
-
-console.log(str);
-
+ let result  = String(num).split('').reduce((sum, num) => sum*num);
+ result **= 3;
+ console.log(String(result).slice(0,2));
+ 
 
