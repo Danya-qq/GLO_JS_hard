e646fa18fd7cca017820bb28c58900a1a7255377
Danya-qq <qallandbb@gmail.com>
2020-03-30 19:21:50 +0300

    lesson3 is done

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-let num = 266219;
+// 1)
 
- let result  = String(num).split('').reduce((sum, num) => sum*num);
- result **= 3;
- console.log(String(result).slice(0,2));
- 
+let lang = 'en',
+daysRu = ['Понедельник', 'Вторник', 'Среда', 'Чертверг', 'Пятница',
+'Суббота', 'Воскресенье'],
+daysEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+'Thuesday', 'Friday', 'saturday'];
+
+ if (lang == 'ru') {
+     console.log(daysRu);
+ } else {
+     console.log(daysEn);
+     
+ };
+
+ switch (lang) {
+     case 'ru':
+         console.log(daysRu);
+        break;
+      case 'en':
+          console.log(daysEn);
+          break;   
+ };
+
+let arrDays = [[daysEn], [daysRu]];
+let result = lang == 'en' ? arrDays[0] : arrDays[1];
+
+console.log(result); // правильно ли я понял условие задания?
+
+// 2)
+
+let namePerson = 'Даня';
+
+let name = (namePerson == 'Артем') ? 'Директор' : 
+(namePerson == 'Максим') ? 'Преподаватель' : 'студент';
+
+console.log(name);
 
